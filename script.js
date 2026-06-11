991477d102943f5e8a12954c609b2df9ec7fe150
hallmarkweb12 <adejokebab@yahoo.com>
2026-06-11 13:26:55 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,48 @@
 const text = "Frontend Developer • UI Builder • Problem Solver";
 let i = 0;
 let el = document.querySelector(".typing");
-function type(){
-    if(!el) return;
-    if(i<text.length){
+function type() {
+    if (!el) return;
+    if (i < text.length) {
         el.textContent += text.charAt(i);
         i++;
-        setTimeout(type,100);
+        setTimeout(type, 100);
     }
 }
 type();
+const form = document.getElementById('form');
+const submitBtn = form.querySelector('button[type="submit"]');
+
+form.addEventListener('submit', async (e) => {
+    e.preventDefault();
+
+    const formData = new FormData(form);
+    formData.append("access_key", "01062a91-6e61-4609-a950-7f781ee3daa7");
+
+    const originalText = submitBtn.textContent;
+
+    submitBtn.textContent = "Sending...";
+    submitBtn.disabled = true;
+
+    try {
+        const response = await fetch("https://api.web3forms.com/submit", {
+            method: "POST",
+            body: formData
+        });
+
+        const data = await response.json();
+
+        if (response.ok) {
+            alert("Success! Your message has been sent.");
+            form.reset();
+        } else {
+            alert("Error: " + data.message);
+        }
+
+    } catch (error) {
+        alert("Something went wrong. Please try again.");
+    } finally {
+        submitBtn.textContent = originalText;
+        submitBtn.disabled = false;
+    }
+});
